ce7348dec9c5047eb836106bad69b05eff45b2e6
Nick <dibartolonick0518@gmail.com>
2022-09-15 14:00:21 -0400

    created emTracker_db and 3 tables in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,24 @@
-CREATE DATABASE x_db;
+CREATE DATABASE emTracker_db;
+
+USE emTracker_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+) 
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL
+
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+)
