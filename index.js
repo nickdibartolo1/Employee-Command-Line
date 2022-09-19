9c3f9045c41fad5974dd0ab8d7e0148925f400f3
Nick <dibartolonick0518@gmail.com>
2022-09-19 13:54:35 -0400

    added .env file, edits to index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const inquier = require('inquirer')
+const inquire = require('inquirer')
 const mysql = require('mysql2')
+require('dotenv').config();
+
+
+const connection = mysql.createConnection(
+    {
+        host: "localhost",
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        db: process.env.DATABASE
+},
+console.log("Ready to go!")
+)
