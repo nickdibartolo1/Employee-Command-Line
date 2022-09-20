c456a4671f7c6f789c4f4d54f31d38301961ce08
Nick <dibartolonick0518@gmail.com>
2022-09-20 12:23:07 -0400

    added switch statements to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquire = require('inquirer')
+const inquier = require('inquirer');
 const mysql = require('mysql2')
 require('dotenv').config();
 
@@ -9,6 +9,52 @@ const connection = mysql.createConnection(
         user: process.env.USER,
         password: process.env.PASSWORD,
         db: process.env.DATABASE
-},
-console.log("Ready to go!")
+    },
+    console.log("Ready to go!")
 )
+
+
+function homeMenu() {
+    inquier
+        .prompt([
+            {
+                type: "list",
+                name: "choices",
+                message: "Choose what you would like to do",
+                choices: [
+                    "Add department",
+                    "Add role",
+                    "Add employee",
+                    "View departments",
+                    "View roles",
+                    "View employees",
+                    "Update employee role",
+                    "Exit"
+                ],
+            },
+        ])
+        .then(({ option }) => {
+            switch (option) {
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "View Departments":
+                    viewDepartments();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "View Employees":
+                    viewEmployees();
+                    break;
+                default:
+                    exit();
+            }
+        });
+}
